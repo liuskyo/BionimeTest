3efa1a30f0e7ad1499112e015a0cedd9edec940c
劉天佑 <gn00595067@gmail.com>
2016-03-16 19:02:36 +0800

    增加必填在model上

--- app/models/nurse.rb
+++ app/models/nurse.rb
@@ -1,4 +1,5 @@
 class Nurse < ActiveRecord::Base
+	validates_presence_of :name,:number
 	has_many :station_nurseships
 	has_many :stations,:through=>:station_nurseships
 end
--- app/models/station.rb
+++ app/models/station.rb
@@ -1,4 +1,5 @@
 class Station < ActiveRecord::Base
+	validates_presence_of :name
 	has_many :station_nurseships
 	has_many :nurses,:through=>:station_nurseships
 end
