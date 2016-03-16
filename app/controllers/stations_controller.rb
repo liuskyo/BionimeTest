7b9dea67d30331a71fbe4ea175b603f752d30e6d
劉天佑 <gn00595067@gmail.com>
2016-03-16 19:50:37 +0800

    新增 站點列表 和 刪除站點功能

--- app/controllers/stations_controller.rb
+++ app/controllers/stations_controller.rb
@@ -37,7 +37,10 @@ class StationsController < ApplicationController
 	end
 
 	def destroy
-		
+		@station = Station.find(params[:id])
+		@station.destroy
+
+		redirect_to stations_path
 	end
 
 	private
