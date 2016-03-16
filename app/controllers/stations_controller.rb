290c56cae2b085bb2390b3ebb5937115fd32e2d8
劉天佑 <gn00595067@gmail.com>
2016-03-16 18:55:40 +0800

    增加站點新增功能

--- app/controllers/stations_controller.rb
+++ app/controllers/stations_controller.rb
@@ -5,8 +5,42 @@ class StationsController < ApplicationController
 
 
 	def index
-		@stations=Station.all
+		@stations = Station.all
 	end
 
+	def show
+		
+	end
+
+	def new
+		@station = Station.new
+	end
+
+	def create
+		@station = Station.new(station_params)
+		if @station.save
+			redirect_to station_path(@station)
+		else
+			render new_station_path
+		end
+	end
+
+	def edit
+		
+	end
+
+	def update
+		
+	end
+
+	def destroy
+		
+	end
+
+	private
+
+	def station_params
+		params.require(:station).permit(:name,:number,:nurse_id)
+	end
 
 end
